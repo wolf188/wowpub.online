95b9c7680dcba2b8a888f08d5de26d03a5d0149f
dsj <375469806@qq.com>
2016-08-15 17:03:41 +0800

    connect mysql

--- wowpub.cn/src/main/java/wowpub/cn/CreateUser.java
+++ wowpub.cn/src/main/java/wowpub/cn/CreateUser.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.sql.*;
 
 /**
  * Servlet implementation class CreateUser
@@ -28,6 +29,29 @@ public class CreateUser extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			System.out.println("loading mysql driver !");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try{
+			Connection connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/realmd1", "root", "mangos");
+			System.out.println("Connect mysql !");
+			Statement state = connection.createStatement();
+			ResultSet rs = state.executeQuery("select username, gmlevel from account;");
+			while(rs.next()){
+				System.out.println(rs.getString("username"));
+				response.getWriter().append("username = ").append(rs.getString("username")).append("    ");
+				response.getWriter().append("gmlevel = ").append(rs.getString("gmlevel")).append("\r\n");
+				
+			}
+		}
+		catch(Exception e){
+			System.out.println(e.getMessage());
+			e.printStackTrace();	
+		}
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
